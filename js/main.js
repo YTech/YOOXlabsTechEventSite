5283be169f08aa4167ecc819495813e3e2de9fae
Erika Tebaldi <tech683@yoox.com>
2015-10-29 17:47:56 +0100

    first layer
    implemented basic functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).ready(
 
+    function () {
+
+        $("html").niceScroll();
+
+    }
+
+);
+
+$('a[href*=#]').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        $('nav li').removeClass('active');
+        $(this).parent().addClass('active');
+        var $target = $(this.hash);
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+        if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body').animate({
+                scrollTop: targetOffset
+            }, 800);
+            return false;
+        }
+    }
+});
