29ce3df20fd98cc78e417ac19e2e5d116069d5bf
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2015-10-30 22:53:53 +0100

    Simplified js

--- js/main.js
+++ js/main.js
@@ -1,126 +1,115 @@
-$('a[href*=#]').click(function () {
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		$('nav li').removeClass('active');
-		$(this).parent().addClass('active');
-		if ($('nav').hasClass('open')) {
-
-			$('nav').slideUp().removeClass('open');
-
-		}
-		;
-		var $target = $(this.hash);
-		$target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-		if ($target.length) {
-			var targetOffset = $target.offset().top;
-			$('html,body').animate({
-				scrollTop: targetOffset
-			}, 800);
-			return false;
-		}
-	}
-});
-
-$('.btn-responsive-menu').click(function () {
+(function () {
 	var $nav = $('nav');
-	if ($nav.hasClass('open')) {
-		$nav.slideUp().removeClass('open');
-	} else {
-		$nav.slideDown().addClass('open');
-	}
-});
+	$('a[href*=#]').on('click', function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			$(this).parent()
+				.addClass('active')
+				.siblings()
+				.removeClass('active');
+			if ($nav.hasClass('open')) {
+				$nav.slideUp().removeClass('open');
+			}
 
-google.maps.event.addDomListener(window, 'load', init);
-var map;
-function init() {
-	var mapOptions = {
-		center: new google.maps.LatLng(44.494551, 11.238763),
-		zoom: 15,
-		zoomControl: true,
-		zoomControlOptions: {
-			style: google.maps.ZoomControlStyle.SMALL
-		},
-		disableDoubleClickZoom: true,
-		mapTypeControl: true,
-		mapTypeControlOptions: {
-			style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-		},
-		scaleControl: true,
-		scrollwheel: false,
-		panControl: true,
-		streetViewControl: true,
-		draggable: true,
-		overviewMapControl: true,
-		overviewMapControlOptions: {
-			opened: true
-		},
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapElement = document.getElementById('map');
-	var map = new google.maps.Map(mapElement, mapOptions);
-	new google.maps.Marker({
-		icon: 'https://mapbuildr.com/assets/img/markers/default.png',
-		position: new google.maps.LatLng(44.495823272909256, 11.238436236770667),
-		map: map,
-		title: "Yoox Net-a-Porter Group Headquarters"
+			var $target = $(this.hash);
+			$target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+			if ($target.length) {
+				var targetOffset = $target.offset().top;
+				$('html,body').animate({
+					scrollTop: targetOffset
+				}, 800);
+				return false;
+			}
+		}
 	});
-}
 
-/**
- * modalEffects.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-(function ($) {
+	$('.btn-responsive-menu').click(function () {
+		if ($nav.hasClass('open')) {
+			$nav.slideUp().removeClass('open');
+		} else {
+			$nav.slideDown().addClass('open');
+		}
+	});
+}());
 
+(function () {
+	google.maps.event.addDomListener(window, 'load', init);
 	function init() {
+		var mapOptions = {
+			center: new google.maps.LatLng(44.494551, 11.238763),
+			zoom: 15,
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.SMALL
+			},
+			disableDoubleClickZoom: true,
+			mapTypeControl: true,
+			mapTypeControlOptions: {
+				style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+			},
+			scaleControl: true,
+			scrollwheel: false,
+			panControl: true,
+			streetViewControl: true,
+			draggable: true,
+			overviewMapControl: true,
+			overviewMapControlOptions: {
+				opened: true
+			},
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var mapElement = document.getElementById('map');
+		var map = new google.maps.Map(mapElement, mapOptions);
+		new google.maps.Marker({
+			icon: 'https://mapbuildr.com/assets/img/markers/default.png',
+			position: new google.maps.LatLng(44.495823272909256, 11.238436236770667),
+			map: map,
+			title: "Yoox Net-a-Porter Group Headquarters"
+		});
+	}
+}());
 
-		var overlay = document.querySelector('.md-overlay');
+(function () {
 
-		[].slice.call(document.querySelectorAll('.md-trigger')).forEach(function (el, i) {
+	var overlay = document.querySelector('.md-overlay');
 
-			var $modal = $('#' + el.getAttribute('data-modal')),
-				$close = $modal.find('.md-close'),
-				$document = $(document),
-				$el = $(el);
+	$('.md-trigger').each(function(){
+		var $this = $(this),
+			$modal = $('#' + $this.attr('data-modal')),
+			$close = $modal.find('.md-close'),
+			$document = $(document);
 
-			function removeModal(hasPerspective) {
-				$modal.removeClass('md-show');
+		function removeModal(hasPerspective) {
+			$modal.removeClass('md-show');
 
-				if (hasPerspective) {
-					$document.removeClass('md-perspective');
-				}
+			if (hasPerspective) {
+				$document.removeClass('md-perspective');
 			}
+		}
 
-			function removeModalHandler() {
-				removeModal($(el).hasClass('md-setperspective'));
+		function removeModalHandler() {
+			removeModal($this.hasClass('md-setperspective'));
+		}
+
+		$this.on('click', function () {
+			$modal.addClass('md-show');
+			overlay.removeEventListener('click', removeModalHandler);
+			overlay.addEventListener('click', removeModalHandler);
+
+			if ($this.hasClass('md-setperspective')) {
+				setTimeout(function () {
+					$document.addClass('md-perspective');
+				}, 25);
 			}
-
-			$el.on('click', function () {
-				$modal.addClass('md-show');
-				overlay.removeEventListener('click', removeModalHandler);
-				overlay.addEventListener('click', removeModalHandler);
-
-				if ($el.hasClass('md-setperspective')) {
-					setTimeout(function () {
-						$document.addClass('md-perspective');
-					}, 25);
-				}
-			});
-
-			$close.on('click', function (ev) {
-				ev.stopPropagation();
-				removeModalHandler();
-			});
-
 		});
 
-	}
+		$close.on('click', function (ev) {
+			ev.stopPropagation();
+			removeModalHandler();
+		});
+	});
 
-	init();
 
-})(jQuery);
+
+
+
+})();
